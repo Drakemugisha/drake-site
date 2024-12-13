8952cd9b422b9978ce72bfa48fab91d5b972c079
mugisha drake <drakemugisha6@gmail.com>
2024-12-13 11:45:21 +0300

    mugisha

--- dockerfile
+++ dockerfile
@@ -1,5 +1,9 @@
 FROM python:3.10-slim
 
+RUN apt-get update && apt-get install -y \
+    certbot \
+    python3-certbot-nginx
+
 # Set the working directory in the container
 WORKDIR /app
 
--- receive/settings.py
+++ receive/settings.py
@@ -137,5 +137,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:8080',
+    'http://13.51.199.228:8080',
 ]
